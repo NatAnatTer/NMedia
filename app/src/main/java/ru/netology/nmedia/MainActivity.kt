23259aca11be49fbdcc760ff02660d2b7a375359
Natalia <natinho@yandex.ru>
2022-05-03 00:09:33 +0300

    add post, click likes

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,6 +1,48 @@
 package ru.netology.nmedia
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val postExample = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+
+            )
+        binding.render(postExample)
+        binding.post.like.setOnClickListener {
+            postExample.likedByMe = !postExample.likedByMe
+            binding.post.like.setImageResource(getLikeIconResId(postExample.likedByMe))
+        }
+
+    }
+
+    private fun ActivityMainBinding.render(post1: Post) {
+        post.authorName.text = post1.author
+        post.date.text = post1.published
+        post1.content.also { post.postBody.text = it }
+        post.like.setImageResource(getLikeIconResId(post1.likedByMe))
+
+    }
+
+    @DrawableRes
+    private fun getLikeIconResId(liked: Boolean) =
+        if (liked) R.drawable.ic_liked_16 else R.drawable.ic_likes_16
+
+}
+
+//<include
+//android:id="@+id/post"
+//layout="layout/post"/>
