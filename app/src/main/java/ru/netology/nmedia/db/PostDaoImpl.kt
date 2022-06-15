923209cc315124063007323fd829110ec4bd55f6
Natalia <natinho@yandex.ru>
2022-06-15 11:43:25 +0300

    under new fragment, repost, like, find post by id, complete

--- app/src/main/java/ru/netology/nmedia/db/PostDaoImpl.kt
+++ app/src/main/java/ru/netology/nmedia/db/PostDaoImpl.kt
@@ -34,16 +34,6 @@ class PostDaoImpl(
         }
     }
 
-    override fun repost(id: Long) {
-        db.execSQL(
-            """
-               UPDATE "${PostsTable.NAME}" SET
-               repost = repost + 1 
-           """.trimIndent(),
-            arrayOf(id)
-        )
-    }
-
 
     override fun save(post: Post): Post {
         val values = ContentValues().apply {
@@ -76,23 +66,65 @@ class PostDaoImpl(
 
 
     override fun likeById(id: Long) {
-        db.execSQL(
-            """
-               UPDATE "${PostsTable.NAME}" SET
-               likes = likes + CASE WHEN likedByMe THEN -1 ELSE 1 END,
-               likedByMe = CASE WHEN likedByMe THEN 0 ELSE 1 END
-           """.trimIndent(),
-            arrayOf(id)
-        )
-    }
 
-    override fun removeById(id: Long) {
-        db.delete(
+        val post: List<Post> = db.query(
             PostsTable.NAME,
-            "${PostsTable.Column.ID.columnName} = ?",
-            arrayOf(id.toString())
-
-        )
+            PostsTable.ALL_COLUMNS_NAME,
+            "${PostsTable.Column.ID.columnName} =?",
+            arrayOf(id.toString()), null, null, null
+        ).use { cursor ->
+            List(cursor.count) {
+                cursor.moveToNext()
+                cursor.toPost()
+            }
+        }
+        val values = ContentValues().apply {
+            put(PostsTable.Column.LIKES.columnName, if(post[0].likedByMe) post[0].likes - 1 else post[0].likes + 1 )
+            put(PostsTable.Column.LIKED_BY_ME.columnName, if(post[0].likedByMe) 0 else 1 )
+        }
+        if (post[0].id != 0L) {
+            db.update(
+                PostsTable.NAME,
+                values,
+                "${PostsTable.Column.ID.columnName} =?",
+                arrayOf(post[0].id.toString())
+            )
+        }
     }
 
-}
+    override fun repost(id: Long) {
+        val post: List<Post> = db.query(
+            PostsTable.NAME,
+            PostsTable.ALL_COLUMNS_NAME,
+            "${PostsTable.Column.ID.columnName} =?",
+            arrayOf(id.toString()), null, null, null
+        ).use { cursor ->
+            List(cursor.count) {
+                cursor.moveToNext()
+                cursor.toPost()
+            }
+        }
+        val values = ContentValues().apply {
+            put(PostsTable.Column.REPOSTS.columnName, post[0].reposts + 1)
+        }
+        if (post[0].id != 0L) {
+            db.update(
+                PostsTable.NAME,
+                values,
+                "${PostsTable.Column.ID.columnName} =?",
+                arrayOf(post[0].id.toString())
+            )
+        }
+    }
+
+
+        override fun removeById(id: Long) {
+            db.delete(
+                PostsTable.NAME,
+                "${PostsTable.Column.ID.columnName} = ?",
+                arrayOf(id.toString())
+
+            )
+        }
+
+    }
